e483b060396df99048e3f649d1e56c11a2018d8e
indylec <leclercq.indy@gmail.com>
2018-08-13 13:41:02 +0400

    removes postgres db from settings file

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,14 +79,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
-    'postgres': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django_test',
-        'USER': 'indy',
-        'PASSWORD': 'django123',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
 }
 
 
